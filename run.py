8bac2aa5a35a7fd7907573e016c7709be2b4588e
ivanprofpv <akvabanka@gmail.com>
2023-12-26 15:29:12 +0400

    init run bot

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
+import sys
+import asyncio
+import logging
+import os
 
+from aiogram import Bot, Dispatcher
+
+
+
+async def main():
+    bot = Bot(token=os.getenv('BOT_TOKEN'))
+    dp = Dispatcher()
+
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Bot stopped')
