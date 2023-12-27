7e398e27a33be29b33d5976dbc4cfbf948a0b1a9
ivanprofpv <akvabanka@gmail.com>
2023-12-27 11:42:48 +0400

    fix: syntax correction

---
+++ app/database/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import BigInteger, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+from config import SQLALCHEMY_URL
+
+engine = create_async_engine(SQLALCHEMY_URL, echo=True)  # echo=True it's logging
+
+async_session = async_sessionmaker(engine)
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+
+    products = relationship('Product', back_populates='category')
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    description: Mapped[str] = mapped_column()
+    price: Mapped[int] = mapped_column()
+    category_id: Mapped[int] = mapped_column(ForeignKey('categories.id'))
+
+    category = relationship('Category', back_populates='products')
+
+
+# The function creates tables in the database
+async def async_main():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+        
+
